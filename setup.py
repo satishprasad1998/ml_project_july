866fe80dfb87b375e8bfc8049f48d223a1b5c525
satishprasad1998 <s.prasad@iitg.ac.in>
2023-07-18 00:56:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_DOT_E='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requiremnts 
+    '''
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPHEN_DOT_E in requirements:
+            requirements.remove(HYPHEN_DOT_E)
+
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Satish',
+author_email='s.prasad@iitg.ac.in',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+)
